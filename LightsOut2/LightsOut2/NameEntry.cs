a4ed8258dca0466e3d51f9bcb63d24deed8167fb
Patrik Nilsson <patriknilson@hotmail.com>
2018-04-05 14:34:36 +0200

    Changes to Highscore

---
+++ LightsOut2/LightsOut2/NameEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace LightsOut2
+{
+    static class NameEntry
+    {
+        //static char[] alphabet = new char['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+        //public static void Draw(SpriteBatch spriteBatch)
+        //{
+            
+        //}
+    }
+}
