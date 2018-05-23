f9889edd18431889669c00de313b291e07f10679
Alexander Flodhag <alexander_flodhag@hotmail.com>
2018-05-23 22:13:16 +0200

    Mini

--- LightsOut2/LightsOut2/Game1.cs
+++ LightsOut2/LightsOut2/Game1.cs
@@ -62,9 +62,6 @@ namespace LightsOut2
 
         protected override void Update(GameTime gameTime)
         {
-            //if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            //    Exit();
-
             Constants.UpdateKeyMouseReader();
 
             switch (currentState)
